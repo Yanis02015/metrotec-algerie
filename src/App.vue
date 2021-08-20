98cd0f553e2ce920af148a257733e881c58cee96
Yanis OULHACI <yaniscrab@gmail.com>
2021-08-20 09:34:55 +0100

    Header v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,139 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      absolute
+      app
+      color="grey"
+      dark
+      height="180"
+      src="https://images.unsplash.com/photo-1582738411706-bfc8e691d1c2?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
+    >
+      <v-row>
+        <v-col cols="12" class="d-flex justify-center align-center">
+          <div class="d-flex align-center">
+            <v-img
+              v-if="false"
+              alt="Vuetify Logo"
+              class="shrink mr-2"
+              contain
+              src="./assets/metrotec-logo.png"
+              transition="scale-transition"
+              width="30"
+            />
+
+            <v-img
+              v-if="true"
+              alt="Metrotec Logo"
+              class="shrink mt-1"
+              transition="scale-transition"
+              contain
+              min-width="100"
+              src="./assets/metrotec-logo-name.png"
+              width="250"
+            />
+          </div>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+    <v-main>
+      <v-bottom-navigation
+        :background-color="color"
+        v-model="value"
+        grow
+        dark
+        :horizontal="!$vuetify.breakpoint.mobile"
+        :shift="$vuetify.breakpoint.smAndDown"
+        :fixed="$vuetify.breakpoint.smAndDown"
+      >
+        <v-btn>
+          <span>Accueil</span>
+
+          <v-icon class="mb-1">mdi-home</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Actualités</span>
+
+          <v-icon class="mb-1">mdi-newspaper</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Présentation</span>
+
+          <v-icon class="mb-1">mdi-creation</v-icon>
+        </v-btn>
+
+        <v-menu
+          rounded
+          offset-y
+          open-on-hover
+          :top="$vuetify.breakpoint.smAndDown"
+        >
+          <template v-slot:activator="{ attrs, on }">
+            <v-btn v-bind="attrs" v-on="on">
+              <span>
+                Activités
+                <v-icon size="20">
+                  {{
+                    $vuetify.breakpoint.smAndDown
+                      ? "mdi-chevron-up"
+                      : "mdi-chevron-down"
+                  }}
+                </v-icon>
+              </span>
+              <v-icon class="mb-1">mdi-check-all</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list >
+            <v-list-item v-for="item in items" :key="item" link >
+              <v-list-item-title>{{ item }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
+        <v-btn>
+          <span>Contact</span>
+
+          <v-icon class="mb-1">mdi-email</v-icon>
+        </v-btn>
+      </v-bottom-navigation>
+      <NavBar />
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    value: 1,
+    items: [
+      "Contrôle technique APV/APG/APL/APE",
+      "Etalonnage et Tarage soupapes",
+      "Contrôle Non Destructif",
+      "Etude d'impact sur l'environnement PSI-EDD",
+      "Expertise maritimes et industrielles",
+      "Accompagnement et Formations",
+    ],
+  }),
+  computed: {
+    color() {
+      switch (this.value) {
+        case 0:
+          return "blue-grey";
+        case 1:
+          return "teal";
+        case 2:
+          return "brown";
+        case 3:
+          return "indigo";
+        default:
+          return "blue-grey";
+      }
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container fluid flat class="pa-0">
+    <v-sheet height="1000">
+      <v-container height="1000"></v-container>
+    </v-sheet>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
