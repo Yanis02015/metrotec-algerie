de37405e8866deb6894c74f71dd525ae677ac579
Yanis OULHACI <yaniscrab@gmail.com>
2021-08-21 12:11:13 +0100

    home v1.0

--- src/App.vue
+++ src/App.vue
@@ -5,37 +5,48 @@
       app
       color="grey"
       dark
-      height="180"
+      :height="$vuetify.breakpoint.smAndDown ? 60 : 130"
       src="https://images.unsplash.com/photo-1582738411706-bfc8e691d1c2?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
     >
       <v-row>
         <v-col cols="12" class="d-flex justify-center align-center">
           <div class="d-flex align-center">
             <v-img
-              v-if="false"
-              alt="Vuetify Logo"
-              class="shrink mr-2"
-              contain
-              src="./assets/metrotec-logo.png"
-              transition="scale-transition"
-              width="30"
-            />
-
-            <v-img
-              v-if="true"
+              v-if="!$vuetify.breakpoint.smAndDown"
               alt="Metrotec Logo"
               class="shrink mt-1"
               transition="scale-transition"
               contain
               min-width="100"
               src="./assets/metrotec-logo-name.png"
-              width="250"
+              width="200"
             />
           </div>
         </v-col>
       </v-row>
     </v-app-bar>
-    <v-main>
+
+    <v-main :class="$vuetify.breakpoint.smAndDown ? 'mb-14' : 'mb-0'">
+      <!-- v-if="$vuetify.breakpoint.smAndDown" -->
+      <v-card
+        v-if="$vuetify.breakpoint.smAndDown"
+        elevation="5"
+        class="black d-flex justify-center align-center"
+        width="100%"
+        height="10"
+        tile
+      >
+        <v-card id="logoTopOnMobile" elevation="5">
+          <v-img
+            v-if="true"
+            alt="Metrotec Logo"
+            class="shrink ma-2"
+            transition="scale-transition"
+            src="./assets/metrotec-logo-name.png"
+            width="200"
+          />
+        </v-card>
+      </v-card>
       <v-bottom-navigation
         :background-color="color"
         v-model="value"
@@ -45,19 +56,20 @@
         :shift="$vuetify.breakpoint.smAndDown"
         :fixed="$vuetify.breakpoint.smAndDown"
       >
-        <v-btn>
+      
+        <v-btn @click="goToHome">
           <span>Accueil</span>
 
           <v-icon class="mb-1">mdi-home</v-icon>
         </v-btn>
 
-        <v-btn>
+        <v-btn @click="goToNews">
           <span>Actualités</span>
 
           <v-icon class="mb-1">mdi-newspaper</v-icon>
         </v-btn>
 
-        <v-btn>
+        <v-btn @click="goToPresentation">
           <span>Présentation</span>
 
           <v-icon class="mb-1">mdi-creation</v-icon>
@@ -70,7 +82,7 @@
           :top="$vuetify.breakpoint.smAndDown"
         >
           <template v-slot:activator="{ attrs, on }">
-            <v-btn v-bind="attrs" v-on="on">
+            <v-btn v-bind="attrs" v-on="on" @click="goToActivities">
               <span>
                 Activités
                 <v-icon size="20">
@@ -85,21 +97,154 @@
             </v-btn>
           </template>
 
-          <v-list >
-            <v-list-item v-for="item in items" :key="item" link >
-              <v-list-item-title>{{ item }}</v-list-item-title>
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index" link>
+              <v-list-item-title @click="goToActivitiesMenu(index)">{{ item }}</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
 
-        <v-btn>
+        <v-btn @click="goToContact">
           <span>Contact</span>
 
           <v-icon class="mb-1">mdi-email</v-icon>
         </v-btn>
       </v-bottom-navigation>
-      <NavBar />
       <router-view />
+
+      <v-footer dark padless class="mt-9">
+        <v-card flat tile class="white--text text-center">
+          <v-img
+            class="text-center align-center"
+            dark
+            src="https://www.beamex.com/wp-content/uploads/2017/05/footerbg-1600x816.jpg"
+            gradient="to top left, rgba(249, 168, 37,.3), rgba(0, 0, 0,.8)"
+          >
+            <v-card class="transparent" tile elevation="0">
+              <router-link to="/">
+                <v-hover v-slot="{ hover }">
+                  <v-card
+                    id="logo-footer"
+                    :elevation="hover ? '7' : '3'"
+                    class="mx-auto mt-9 pa-3"
+                    width="270"
+                  >
+                    <v-img class="ma-auto" width="100%" src="./assets/metrotec-logo-name.png"/>
+                  </v-card>
+                </v-hover>
+              </router-link>
+              <p style="font-size: 20px" class="mt-5">
+                Nous existons pour fournir de meilleurs moyens de calibrer.
+              </p>
+            </v-card>
+
+            <v-divider class="mx-16"></v-divider>
+
+            <v-card-text>
+              <v-btn
+                v-for="icon in icons"
+                :key="icon.name"
+                class="mx-4 white--text"
+                icon
+                plain
+              >
+                <v-hover v-slot="{ hover }">
+                  <v-icon :color="hover ? icon.color : ''" size="24px">
+                    {{ icon.name }}
+                  </v-icon>
+                </v-hover>
+              </v-btn>
+            </v-card-text>
+
+            <v-row dark>
+              <v-hover v-slot="{ hover }">
+                <v-col cols="12" md="4">
+                  <v-card-title class="text-h4">Qui sommes nous ?</v-card-title>
+                  <v-card
+                    height="4"
+                    :width="hover ? '320' : '50'"
+                    class="ml-4 mb-5 white smooth-transaction"
+                    :class="color"
+                    style="margin-top: -8px; transition: all 1.2s;"
+                    flat
+                  ></v-card>
+                  <v-hover v-slot="{ hover }">
+                    <v-card-text
+                      :class="
+                        hover ? 'white--text' : 'grey--text text--lighten-1'
+                      "
+                      class="pt-0 text-left"
+                    >
+                      Phasellus feugiat arcu sapien, et iaculis ipsum elementum
+                      sit amet. Mauris cursus commodo interdum. Praesent ut
+                      risus eget metus luctus accumsan id ultrices nunc.
+                      <br /><br />
+                      Sed at orci sed massa consectetur dignissim a sit amet
+                      dui. Duis commodo vitae velit et faucibus. Morbi vehicula
+                      lacinia malesuada. Nulla placerat augue vel ipsum
+                      ultrices, cursus iaculis dui sollicitudin. Vestibulum eu
+                      ipsum vel diam elementum tempor vel ut orci. Orci varius
+                      natoque penatibus et magnis dis parturient montes,
+                      nascetur ridiculus mus.
+                    </v-card-text>
+                  </v-hover>
+                </v-col>
+              </v-hover>
+              <v-col cols="12" md="4"></v-col>
+              <v-col cols="12" md="4"></v-col>
+            </v-row>
+
+            <v-divider></v-divider>
+
+            <v-row class="align-center">
+              <v-col cols="12" md="4">
+                <v-card-text>
+                  <div
+                    :class="
+                      $vuetify.breakpoint.smAndDown
+                        ? 'justify-center'
+                        : 'justify-start'
+                    "
+                    class="d-flex align-center"
+                  >
+                    <v-avatar class="mr-3" size="40">
+                      <img
+                        alt="yanis.indt"
+                        src="./assets/yanis-indt-logo.jpg"
+                      />
+                    </v-avatar>
+                    <v-card class="transparent" tile elevation="0">
+                      <v-list-item-content
+                        style="width: 100px;"
+                        class="text-left py-0"
+                      >
+                        <v-list-item-title class="py-0"
+                          >Réalisation</v-list-item-title
+                        >
+                        <v-list-item-subtitle class="py-0">
+                          <a
+                            target="_blank"
+                            class="pink--text text--lighten-2"
+                            href="https://instagram.com/yanis.indt"
+                            >@yanis.indt</a
+                          >
+                        </v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-card>
+                  </div>
+                </v-card-text>
+              </v-col>
+
+              <v-col class="pa-0 ma-0" cols="12" md="4">
+                <v-card-text class="white--text">
+                  &#169; {{ new Date().getFullYear() }} Métrotech Algérie —
+                  <strong>Tous Droits Réservés.</strong>
+                </v-card-text>
+              </v-col>
+            </v-row>
+          </v-img>
+        </v-card>
+      </v-footer>
     </v-main>
   </v-app>
 </template>
@@ -109,7 +254,25 @@ export default {
   name: "App",
 
   data: () => ({
-    value: 1,
+    icons: [
+      {
+        name: "mdi-facebook",
+        color: "blue darken-3",
+      },
+      {
+        name: "mdi-twitter",
+        color: "light-blue accent-3",
+      },
+      {
+        name: "mdi-linkedin",
+        color: "light-blue darken-2",
+      },
+      {
+        name: "mdi-instagram",
+        color: "pink accent-4",
+      },
+    ],
+    value: 0,
     items: [
       "Contrôle technique APV/APG/APL/APE",
       "Etalonnage et Tarage soupapes",
@@ -119,11 +282,31 @@ export default {
       "Accompagnement et Formations",
     ],
   }),
+  methods: {
+    goToHome() {
+      this.$router.replace('/')
+    },
+    goToNews() {
+      this.$router.replace('/news')
+    },
+    goToPresentation() {
+      this.$router.replace('/presentation')
+    },
+    goToActivities() {
+      this.$router.replace('/activities')
+    },
+    goToActivitiesMenu(i) {
+      console.log('i = ', i);
+    },
+    goToContact() {
+      this.$router.replace('/contact')
+    }
+  },
   computed: {
     color() {
       switch (this.value) {
         case 0:
-          return "blue-grey";
+          return "yellow darken-3";
         case 1:
           return "teal";
         case 2:
@@ -134,6 +317,49 @@ export default {
           return "blue-grey";
       }
     },
+    colorFooter() {
+      switch (this.value) {
+        case 0:
+          return "yellow darken-2";
+        case 1:
+          return "teal lighten-2";
+        case 2:
+          return "brown lighten-1";
+        case 3:
+          return "indigo lighten-1";
+        default:
+          return "blue-grey lighten-1";
+      }
+    },
   },
 };
 </script>
+
+<style>
+p,
+span {
+  font-family: "Roboto", sans-serif;
+}
+a {
+  color: pink;
+  text-decoration: none;
+}
+#logoTopOnMobile {
+  background: no-repeat center/200%
+    url("https://images.unsplash.com/photo-1582738411706-bfc8e691d1c2?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80");
+  position: absolute;
+  z-index: 99;
+  border: 3px solid white;
+}
+
+#logo-footer {
+  background: no-repeat center/120%
+    url("https://images.unsplash.com/photo-1582738411706-bfc8e691d1c2?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80");
+  transition: all 1s;
+}
+
+#logo-footer:hover {
+  background: no-repeat center/300%
+    url("https://images.unsplash.com/photo-1582738411706-bfc8e691d1c2?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80");
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,238 @@
 <template>
-  <v-container fluid flat class="pa-0">
-    <v-sheet height="1000">
-      <v-container height="1000"></v-container>
-    </v-sheet>
+  <v-container fluid flat class="pa-0 ma-0">
+    <v-container fluid class="pa-0">
+      <v-hover v-slot="{ hover }">
+        <v-card tile class="ma-0 pa-0">
+          <v-img
+            class="text-center align-center"
+            dark
+            height="450"
+            src="https://images.unsplash.com/photo-1460925895917-afdab827c52f"
+            gradient="to top left, rgba(0, 106, 255,.3), rgba(0, 0, 0,.8)"
+          >
+            <p
+              :class="
+                $vuetify.breakpoint.mobile
+                  ? 'slogan-size-mobile'
+                  : 'slogan-size'
+              "
+              class="slogan"
+            >
+              Etalonner de manière toujours plus innovante et efficace
+            </p>
+            <p class="slogan subtitle-slogan">
+              Découvrez de meilleures manières d’étalonner
+            </p>
+            <v-card
+              height="4"
+              :width="hover ? '300' : '17'"
+              class="ma-auto white smooth-transaction yellow darken-3"
+              flat
+            ></v-card>
+          </v-img>
+        </v-card>
+      </v-hover>
+    </v-container>
+
+    <v-divider class="my-12 mx-12"></v-divider>
+
+    <v-container fluid class="ma-0 pa-0">
+      <h1 class="text-center text-decoration-underline title-partnaires">
+        Nos partnaires
+      </h1>
+      <v-slide-group class="pa-4" :show-arrows="$vuetify.breakpoint.xs ? false : true">
+        <v-slide-item
+          cycle
+          v-for="partnaire in partnaires"
+          :key="partnaire.nom"
+        >
+          <v-card class="ma-2 pa-2" elevation="1">
+            <v-img
+              :width="$vuetify.breakpoint.mobile ? 60 : 120"
+              :src="partnaire.url"
+            />
+          </v-card>
+        </v-slide-item>
+      </v-slide-group>
+    </v-container>
+
+    <v-carousel
+      cycle
+      height="400"
+      hide-delimiter-background
+      show-arrows-on-hover
+    >
+      <v-carousel-item
+        align="center"
+        v-for="(slide, i) in activiteSlides"
+        :key="i"
+        :src="slide.url"
+      >
+        <v-sheet class="transparent d-flex justify-center align-center" dark height="100%" width="100%">
+          <v-sheet class="d-flex justify-center align-center carrousel-text-background" height="50" width="100%">
+            <span>{{ slide.title }}</span>
+          </v-sheet>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+
+    <!-- -->
+    <v-divider class="my-12 mx-12"></v-divider>
+
+    <v-container class="text-center ma-auto pa-0">
+      <h1 class="text-decoration-underline title-partnaires mb-5">
+        Metrotec Algerie
+      </h1>
+      <v-card class="ma-auto" flat width="80%">
+        <span>Lorem ipsum dolor sit amet consectetur <strong>selit</strong>. Laborum cupiditate, minima sit soluta explicabo exercitationem ipsam vel esse labore aut nesciunt, quo iusto deleniti necessitatibus veritatis libero dignissimos adipisci sed. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga sed hic suscipit possimus consectetur sit facilis laudantium architecto eaque officiis? Voluptatibus, asperiores. Quaerat unde obcaecati, error praesentium a iste assumenda! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, animi totam repellendus quam, commodi tempore libero praesentium odit non, harum error in eaque temporibus doloremque minus voluptatem exercitationem laudantium possimus!</span>
+      </v-card>
+    </v-container>
   </v-container>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      model: 0,
+      activiteSlides: [
+        {
+          title: 'Contrôle technique APV/APG/APL/APE',
+          url: 'https://images.unsplash.com/photo-1579154204845-5d7f8d4dc785?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+        },
+        {
+          title: 'Etalonnage et Tarage soupapes',
+          url: 'https://images.unsplash.com/photo-1581087724844-037b13e16b0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80',
+        },
+        {
+          title: 'Contrôle Non Destructif',
+          url: 'https://images.unsplash.com/photo-1531935015902-64b87c1f4da5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=376&q=80',
+        },
+        {
+          title: "Etude d'impact sur l'environnement PSI-EDD",
+          url: 'https://images.unsplash.com/photo-1578496480240-32d3e0c04525?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+        },
+        {
+          title: 'Expertise maritimes et industrielles',
+          url: 'https://images.unsplash.com/photo-1615906655593-ad0386982a0f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+        },
+        {
+          title: 'Accompagnement et Formations',
+          url: 'https://images.unsplash.com/photo-1621463678002-1a7d3c9224f7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+        },
+      ],
+      partnaires: [
+        {
+          nom: "Buerkert",
+          url: "https://www.lhybride.info/media/images/iconHP/Buerkert.jpg",
+        },
+        {
+          nom: "Socorex Swissth",
+          url:
+            "https://www.lhybride.info/media/images/iconHP/Logo_Socorex_Swissth.jpg",
+        },
+        {
+          nom: "BRAND",
+          url: "https://www.lhybride.info/media/images/iconHP/BRAND.gif",
+        },
+        {
+          nom: "EAS",
+          url: "https://www.lhybride.info/media/images/iconHP/Logo_EAS.jpg",
+        },
+        {
+          nom: "Batailler",
+          url: "https://www.lhybride.info/media/images/iconHP/batailler.jpg",
+        },
+        {
+          nom: "Waters",
+          url: "https://www.lhybride.info/media/images/iconHP/Waters_logo.jpg",
+        },
+        {
+          nom: "Huber",
+          url:
+            "https://www.lhybride.info/media/images/iconHP/Huber_Logo_140.jpg",
+        },
+        {
+          nom: "Anton Paar",
+          url:
+            "https://www.lhybride.info/media/images/iconHP/anton-paar-logo-th.jpg",
+        },
+        {
+          nom: "GERSTEL",
+          url:
+            "https://www.lhybride.info/media/images/iconHP/GERSTEL-140_70.png",
+        },
+        {
+          nom: "Promega",
+          url: "https://www.lhybride.info/media/images/iconHP/PromegaLogo.jpg",
+        },
+        {
+          nom: "VWR Interim",
+          url:
+            "https://www.lhybride.info/media/images/iconHP/VWR_InterimLogo-th.jpg",
+        },
+        {
+          nom: "FST",
+          url: "https://www.lhybride.info/media/images/iconHP/FST-logobth2.jpg",
+        },
+        {
+          nom: "Belleville",
+          url: "https://www.lhybride.info/media/images/iconHP/bellevilleG.jpg",
+        },
+        {
+          nom: "Enzo",
+          url: "https://www.lhybride.info/media/images/iconHP/enzo.png",
+        },
+        {
+          nom: "Lauda",
+          url: "https://www.lhybride.info/media/images/iconHP/lauda18.jpg",
+        },
+        {
+          nom: "BHmettler",
+          url: "https://www.lhybride.info/media/images/iconHP/BHmettler.jpg",
+        },
+      ],
+    };
+  },
 };
 </script>
+
+<style scoped>
+.title-partnaires {
+  font-family: "Cinzel Decorative", cursive;
+  font-size: 25px;
+}
+
+.slogan {
+  font-weight: 700;
+  text-shadow: 1px 1px 3px #000000;
+}
+
+.subtitle-slogan {
+  font-size: 17px;
+}
+
+.slogan-size-mobile {
+  font-size: 35px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.slogan-size {
+  font-size: 58px;
+  margin-left: 90px;
+  margin-right: 90px;
+}
+
+.smooth-transaction {
+  transition: all 1.2s;
+}
+
+.carrousel-text-background {
+  margin-top: 125px;
+  background: rgba(61, 61, 61, 0.5);
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
